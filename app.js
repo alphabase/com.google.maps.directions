ae79bfeabdde97348ce2601b59a51c4d07350fba
Gerard Nijboer <me@gerardnijboer.com>
2017-02-09 22:34:10 +0100

    Implemented action and trigger cards.
    The proposed action card and trigger card to update traveling details to
    work address have been implemented.
    In order to make sure the context of the tokens is explicit for the end
    user, only a single work address is supported. Multiple addresses can be
    supported in the future. Both the trigger tokens as general tokens are
    updated so that users can create their own voice messages.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var tokens = [];
 
 function init() {
 	Homey.log('Google maps directions app ready!');
+	registerToken('summary', 'string', '', function(err, token) { tokens['summary'] = token; });
+	registerToken('duration_in_traffic', 'number', 0, function(err, token) { tokens['duration_in_traffic'] = token; });
+	registerToken('duration', 'number', 0, function(err, token) { tokens['duration'] = token; });
+	registerToken('distance', 'number', 0, function(err, token) { tokens['distance'] = token; });
 }
 
 // Driving instructions
@@ -15,12 +19,7 @@ Homey.manager('flow').on('action.com_google_maps_directions_driving', function(c
 			var duration = data.routes[0].legs[0].duration.text.replace('min', __('min'));
 			var distance = data.routes[0].legs[0].distance.text.replace('km', __('km'));
 			
-			set_token('driving_duration_in_traffic', Number(duration_in_traffic.substr(0,duration_in_traffic.indexOf(' '))));
-			set_token('driving_duration', Number(duration.substr(0,duration.indexOf(' '))));
-			
-			if (Homey.manager('settings').get('enable_speech')) {
-				Homey.manager('speech-output').say(__('resultsDriving', {'summary': summary, 'duration_in_traffic': duration_in_traffic, 'distance': distance, 'duration': duration}));	
-			}
+			Homey.manager('speech-output').say(__('resultsDriving', {'summary': summary, 'duration_in_traffic': duration_in_traffic, 'distance': distance, 'duration': duration}));	
 		}
 	});
 	callback(null, true);
@@ -34,11 +33,7 @@ Homey.manager('flow').on('action.com_google_maps_directions_transit', function(c
 			var departure_time = data.routes[0].legs[0].departure_time.text;
 			var steps = data.routes[0].legs[0].steps.length;
 			
-			set_token('transit_duration', duration);
-			
-			if (Homey.manager('settings').get('enable_speech')) {
-				Homey.manager('speech-output').say(__('resultsTransit', {'duration': duration, 'departure_time': departure_time, 'steps': steps}));
-			}
+			Homey.manager('speech-output').say(__('resultsTransit', {'duration': duration, 'departure_time': departure_time, 'steps': steps}));
 		}
 	});
 	callback(null, true);
@@ -52,11 +47,7 @@ Homey.manager('flow').on('action.com_google_maps_directions_bicycling', function
 			var duration = data.routes[0].legs[0].duration.text.replace('min', __('min'));
 			var distance = data.routes[0].legs[0].distance.text.replace('km', __('km'));
 			
-			set_token('bicycling_duration', duration);
-			
-			if (Homey.manager('settings').get('enable_speech')) {
-				Homey.manager('speech-output').say(__('resultsBicycling', {'summary': summary, 'duration': duration, 'distance': distance}));
-			}
+			Homey.manager('speech-output').say(__('resultsBicycling', {'summary': summary, 'duration': duration, 'distance': distance}));
 		}
 	});
 	callback(null, true);
@@ -70,12 +61,37 @@ Homey.manager('flow').on('action.com_google_maps_directions_walking', function(c
 			var duration = data.routes[0].legs[0].duration.text.replace('min', __('min'));
 			var distance = data.routes[0].legs[0].distance.text.replace('km', __('km'));
 			
-			set_token('walking_duration', duration);
-			
-			if (Homey.manager('settings').get('enable_speech')) {
-				Homey.manager('speech-output').say(__('resultsWalking', {'summary': summary, 'duration': duration, 'distance': distance}));
-			}
+			Homey.manager('speech-output').say(__('resultsWalking', {'summary': summary, 'duration': duration, 'distance': distance}));
 		}
+	});
+	callback(null, true);
+});
+
+// Update details to work
+Homey.manager('flow').on('action.com_google_maps_directions_driving_to_work', function(callback, args) {
+	Homey.manager('geolocation').getLocation(function(err, location) {
+		getRoute(location.latitude + ',' + location.longitude, Homey.manager('settings').get('work_address'), 'driving', function(err, data) {
+			if (!err) {
+				var summary = data.routes[0].summary;
+				var duration_in_traffic = Math.ceil(data.routes[0].legs[0].duration_in_traffic.value / 60);
+				var duration = Math.ceil(data.routes[0].legs[0].duration.value / 60);
+				var distance = Math.ceil(data.routes[0].legs[0].distance.value / 1000);
+				
+				tokens['summary'].setValue(summary, function(err) { if (err) console.error('setValue error:', err); });
+				tokens['duration_in_traffic'].setValue(duration_in_traffic, function(err) { if (err) console.error('setValue error:', err); });
+				tokens['duration'].setValue(duration, function(err) { if (err) console.error('setValue error:', err); });
+				tokens['distance'].setValue(distance, function(err) { if (err) console.error('setValue error:', err); });
+				
+				Homey.manager('flow').trigger('com_google_maps_trigger', {
+					"summary": summary,
+					"duration_in_traffic": duration_in_traffic,
+					"duration": duration,
+					"distance": distance
+				});
+			} else {
+				Homey.manager('speech-output').say(__('errorUnexpected'));
+			}
+		});
 	});
 	callback(null, true);
 });
@@ -130,34 +146,19 @@ function getRoute(origin, destination, mode, callback) {
 	});
 }
 
-function set_token(name, value) {
-	var token = tokens.find(function (dev) {
-		return dev.id == name;
+function registerToken(id, type, value, callback) {
+	Homey.manager('flow').registerToken(id, {
+	    type: type,
+	    title: __('tokens.'+id)
+	}, function(err, token){
+	    if(err) return console.error('registerToken error:', err);
+
+	    token.setValue(value, function(err) {
+	        if(err) return console.error('setValue error:', err);
+	    });
+	    
+	    callback(null, token);
 	});
-	if (token) {
-		token.setValue(value,
-			function (err) {
-			if (err) return console.error('setValue error:', err);
-			Homey.log('Token '+ name +' updated, value: ' + value);
-		});
-		tokens.push(token);
-	} else {
-		Homey.log('Token not found, registering...');
-		
-		Homey.manager('flow').registerToken(name, {
-            type: 'number',
-            title: name
-        }, function (err, token) {
-            if (err) return console.error('registerToken error:', err);
-            
-            token.setValue(value, function (err) {
-                if (err) return console.error('setValue error:', err);
-				Homey.log('Token '+ name +' updated, value: ' + value);
-            });
-            tokens.push(token);
-        });
-        return;
-	}
 }
 
 module.exports.init = init;
